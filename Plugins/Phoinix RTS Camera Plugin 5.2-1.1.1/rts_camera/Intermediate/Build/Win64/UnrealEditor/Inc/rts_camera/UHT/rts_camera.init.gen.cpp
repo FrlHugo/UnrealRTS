e9497ee69c9bd6a8376486558745dc3dfd062a01
Hugo Forel <forel.hugo@gmail.com>
2023-09-12 18:48:34 +0200

    réparation du projet + passage en hybride (blueprint + c++) pour le multi

--- Plugins/Phoinix RTS Camera Plugin 5.2-1.1.1/rts_camera/Intermediate/Build/Win64/UnrealEditor/Inc/rts_camera/UHT/rts_camera.init.gen.cpp
+++ Plugins/Phoinix RTS Camera Plugin 5.2-1.1.1/rts_camera/Intermediate/Build/Win64/UnrealEditor/Inc/rts_camera/UHT/rts_camera.init.gen.cpp
@@ -17,7 +17,7 @@ void EmptyLinkFunctionForGeneratedCoderts_camera_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0xB3CB4808,
+				0xE06CA890,
 				0xC44062FF,
 				METADATA_PARAMS(nullptr, 0)
 			};
@@ -25,5 +25,5 @@ void EmptyLinkFunctionForGeneratedCoderts_camera_init() {}
 		}
 		return Z_Registration_Info_UPackage__Script_rts_camera.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_rts_camera(Z_Construct_UPackage__Script_rts_camera, TEXT("/Script/rts_camera"), Z_Registration_Info_UPackage__Script_rts_camera, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xB3CB4808, 0xC44062FF));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_rts_camera(Z_Construct_UPackage__Script_rts_camera, TEXT("/Script/rts_camera"), Z_Registration_Info_UPackage__Script_rts_camera, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xE06CA890, 0xC44062FF));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
